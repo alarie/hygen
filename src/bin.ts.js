4b61cfc1b624cef09b29d6c3b405690ffc3c51e8
Simon Schmidt <simon-schmidt@alarie.de>
2018-03-04 00:40:30 +0100

    Generate a ts-node version of bin.js

---
+++ src/bin.ts.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env ts-node
+
+const path = require('path')
+const { runner } = require('./index')
+const defaultTemplates = path.join(__dirname, '../src/templates')
+const Logger = require('./logger')
+
+runner(process.argv.slice(2), {
+  templates: defaultTemplates,
+  cwd: process.cwd(),
+  logger: new Logger(console.log.bind(console)),
+  debug: !!process.env.DEBUG,
+  exec: (action, body) => {
+    const opts = body && body.length > 0 ? { input: body } : {}
+    return require('execa').shell(action, opts)
+  }
+})
